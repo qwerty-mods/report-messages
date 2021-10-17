820266b6b39e5018d366d756cedbb376072d7c87
botato <botatostuff@gmail.com>
2021-10-16 21:59:01 -0400

    Add clickable link, <strong> tag and remove unnecessary if statement

--- Reasons.jsx
+++ Reasons.jsx
@@ -1,5 +1,5 @@
-const { React, getModule, i18n: { Messages } } = require('powercord/webpack');
-const { FormTitle, Button } = require('powercord/components');
+const { React, getModule, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack');
+const { AsyncComponent, FormTitle, Button } = require('powercord/components');
 const { RadioGroup } = require('powercord/components/settings');
 const { Modal } = require('powercord/components/modal');
 const { close: closeModal } = require('powercord/modal');
@@ -7,10 +7,13 @@ const { close: closeModal } = require('powercord/modal');
 const { report } = getModule([ 'report', 'submitReport' ], false);
 const { getChannel } = getModule([ 'getChannel' ], false);
 
+const FormText = AsyncComponent.from(getModuleByDisplayName('FormText'));
+
 module.exports = class ReportModal extends React.PureComponent {
     constructor(props) {
         super(props);
 
+        this.classes = Object.values(getModule([ 'anchorUnderlineOnHover' ], false));
         this.state = {
             reason: 'nsfw'
         }
@@ -40,11 +43,16 @@ module.exports = class ReportModal extends React.PureComponent {
                         onChange={(e) => this.setState({reason: e.value})}
                         value={this.state.reason}
                         options={reasons}
-                        required={true}
-                        note={`Reports are sent to the Discord Trust & Safety team${getChannel(message.channel_id).guild_id && ' - not the server owner'}. Creating false reports and/or spamming the report button may result in a suspension of reporting abilities. Learn more from the Discord Community Guidelines. Thanks for keeping things safe and sound.`}
                     >
-                        Choose a Reason
+                        What is it you're reporting?
                     </RadioGroup>
+                    <FormText>
+                        Reports are sent to the Discord Trust & Safety team
+                        {getChannel(message.channel_id).guild_id && <strong> - not the server owner</strong>}.
+                        Creating false reports and/or spamming the report button may result in a suspension of reporting abilities.
+                        Learn more from the <a href='https://discord.com/guidelines' className={this.classes.join(' ')} rel='noreferrer noopener' target='_blank'>Discord Community Guidelines</a>.
+                        Thanks for keeping things safe and sound.
+                    </FormText>
                 </Modal.Content>
 
                 <Modal.Footer>
@@ -59,35 +67,19 @@ module.exports = class ReportModal extends React.PureComponent {
                                     reason: this.state.reason
                                 });
 
-                                console.log(data); // Debug info if something weird happens
+                                powercord.api.notices.sendToast('rmToast', {
+                                    header: 'report',
+                                    content: data.ok ? `Reported ${message.author.username}` : `Failed to report ${message.author.username}`,
+                                    buttons: [{
+                                        text: 'Dismiss',
+                                        color: 'green',
+                                        look: 'outlined',
+                                        onClick: () => powercord.api.notices.closeToast('remountNotif')
+                                    }],
+                                    timeout: 3e4
+                                });
 
-                                if (data.ok) {
-                                    powercord.api.notices.sendToast('rmToast', {
-                                        header: 'report',
-                                        content: `Reported ${message.author.username}`,
-                                        buttons: [{
-                                            text: 'Dismiss',
-                                            color: 'green',
-                                            look: 'outlined',
-                                            onClick: () => powercord.api.notices.closeToast('remountNotif')
-                                        }],
-                                        timeout: 3e4
-                                    });
-                                } else {
-                                    powercord.api.notices.sendToast('rmToast', {
-                                        header: 'report',
-                                        content: `Failed to report ${message.author.username}`,
-                                        buttons: [{
-                                            text: 'Dismiss',
-                                            color: 'red',
-                                            look: 'outlined',
-                                            onClick: () => powercord.api.notices.closeToast('remountNotif')
-                                        }],
-                                        timeout: 3e4
-                                    });
-                                }
-
-                                closeModal()
+                                closeModal();
                             }
                         }
                     >
